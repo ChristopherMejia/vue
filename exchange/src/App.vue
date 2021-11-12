59c2de8536b80a75906661bf805235f38de46bb8
Christopher Mejia <christopher.mejia@altatec.com.mx>
2021-11-12 16:57:32 -0600

    componente header

--- exchange/src/App.vue
+++ exchange/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
+    <px-header />
 
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center"/>
   </main>
 </template>
 
 <script>
+import PxHeader from './components/PxHeader.vue'
 
 export default {
   name: 'App',
-  components: { }
+  components: {PxHeader }
 }
 </script>
 
